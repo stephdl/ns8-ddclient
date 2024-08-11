6bcce324dfd0ce3449753f40afc4e3f7c0411d95
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-08-11 18:18:39 +0200

    chore: Update ddclient configuration to include new fields in Settings.vue

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -80,13 +80,13 @@
               ref="ddclient_password"
             >
             </NsTextInput>
-            <!-- advanced options -->
+            <!-- advanced options 
             <cv-accordion ref="accordion" class="maxwidth mg-bottom">
               <cv-accordion-item :open="toggleAccordion[0]">
                 <template slot="title">{{ $t("settings.advanced") }}</template>
                 <template slot="content"> </template>
               </cv-accordion-item>
-            </cv-accordion>
+            </cv-accordion> -->
             <cv-row v-if="error.configureModule">
               <cv-column>
                 <NsInlineNotification
