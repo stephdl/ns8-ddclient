a70de97e61b71fb258ff1eb2f1660d8db4fac6b4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-03-03 09:10:20 +0100

    Adaptation to ddclient v4 (#12)

--- build-images.sh
+++ build-images.sh
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=0" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/linuxserver/ddclient:4.0.0" \
+    --label="org.nethserver.images=docker.io/linuxserver/ddclient:v4.0.0-ls191" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
